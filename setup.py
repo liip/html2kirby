9477867f6fd68c47a9d65fc50c3c191d7a6a6811
Stefan Heinemann <stefan.heinemann@liip.ch>
2017-08-02 22:03:28 +0200

    Add setup files for Pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'html2kirby',
+  packages = ['html2kirby'],
+  version = '0.1',
+  description = 'A HTML to Kirbytext converter',
+  author = 'Stefan Heinemann',
+  author_email = 'stefan.heinemann@liip.ch.com',
+  url = 'https://github.com/liip/html2kirby',
+  download_url = 'https://github.com/liip/html2kirby/archive/0.1.tar.gz',
+  keywords = ['kirby', 'kirbytext', 'html'],
+  classifiers = [],
+)
